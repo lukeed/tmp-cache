4322e74251836cc4de24500c2a13b3002b5037dc
Luke Edwards <luke.edwards05@gmail.com>
2020-09-08 09:39:24 -0700

    chore(types): spacing

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 declare interface Options {
-    max?: number;
-    maxAge?: number;
-    stale?: boolean;
+	max?: number;
+	maxAge?: number;
+	stale?: boolean;
 }
 
 declare class Cache<K, V> extends Map<K, V> {
-    constructor(options?: Options | number);
-    get(key: K, refresh?: boolean): V | undefined;
-    peek(key: K): V | undefined;
-    set(key: K, value: V, maxAge?: number): this;
+	constructor(options?: Options | number);
+	get(key: K, refresh?: boolean): V | undefined;
+	peek(key: K): V | undefined;
+	set(key: K, value: V, maxAge?: number): this;
 }
 
 export = Cache;
