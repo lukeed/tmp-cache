cb8432d0b62ffe6eb64122948e0ec4770f6b9132
Luke Edwards <luke.edwards05@gmail.com>
2020-09-08 09:27:22 -0700

    Apply suggestions from code review

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
-declare type Options = {
+declare interface Options {
     max?: number;
     maxAge?: number;
     stale?: boolean;
-};
+}
 
 declare class Cache<K, V> extends Map<K, V> {
     constructor(options?: Options | number);
-    get(key: K, update?: boolean): V | undefined;
+    get(key: K, refresh?: boolean): V | undefined;
     peek(key: K): V | undefined;
     set(key: K, value: V, maxAge?: number): this;
 }
